9d103b6d194a65c2d247afaa46c1666b7599a38c
Anatoliy Eristov <anatol262@mail.ru>
2022-12-13 19:10:50 +0300

    Возможность ввода текста

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace ConsoleApp8
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Ввод строк с клавиатуры
+      // и образование нового массива
+
+      // 1. Объявление переменных
+      string Text;
+      string [] SelectedWords = {};
+      // 2. Цикл ввода строк
+      Console.WriteLine("Введите текст:");
+    }
+  }
+}
+    
